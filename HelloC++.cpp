19f21fe6890be7283a694c448f6178c04cf703af
TheFatCow <50284597+TheFatCow@users.noreply.github.com>
2024-03-28 15:03:56 +0530

    done

--- HelloC++.cpp
+++ HelloC++.cpp
@@ -1,5 +1,5 @@
-# include <iostream>
-using namespace std;
+# include <iostream>  // No need to use .h if you use .h that means you are using C hedder files which is bad practice, affects scope of variable
+using namespace std; // when using this you do not need to use std:: again and again
 int main()
 {
     cout<<"Hello C++, my name is Aniket"<<endl;
