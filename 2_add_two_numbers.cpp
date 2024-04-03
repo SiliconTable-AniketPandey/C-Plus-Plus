ed7b006ee8c132f221bdcf971641a75e44112d74
TheFatCow <50284597+TheFatCow@users.noreply.github.com>
2024-04-03 07:51:42 +0530

    done

---
+++ 2_add_two_numbers.cpp
@@ -0,0 +1,10 @@
+# include<iostream>
+using namespace std;
+int main()                  // in newer version of C++ main must return somthing so it cannot be void has it used to be before
+{   // Being a multiple language programmer it is sometimes difficult for me to code in form than 2 languages on the same day
+    int a,b,sum;
+    cout<<"Enter two numbers"<<endl; // << is called the output streaming operator
+    cin>>a>>b;                         // >> is called the input streaming operator
+    sum = a+b;
+    cout<<"Sum = "<<sum;
+}
