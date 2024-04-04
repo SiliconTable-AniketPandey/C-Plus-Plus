090b37ef7f560178a120274fde534422600288e3
TheFatCow <50284597+TheFatCow@users.noreply.github.com>
2024-04-05 02:05:57 +0530

    done

---
+++ 9_strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(void)
+{
+    string str1 = "Hello ";
+    string str2 = "C++";
+
+    string str = str1 + str2; // Concatination just like java and Python
+    cout<<str<<endl;
+}
