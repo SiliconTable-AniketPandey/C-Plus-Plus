ac597496770193c155e898d4e2ada7889ff6c750
TheFatCow <50284597+TheFatCow@users.noreply.github.com>
2024-04-08 21:26:05 +0530

    done

--- 11_sort_stdlib.cpp
+++ 11_sort_stdlib.cpp
@@ -7,7 +7,8 @@ bool compare(int i, int j) {
     return (i < j); // Sort in ascending order;   < for ascending oredr and > for descending order
 }
 
-int main() {
+int main() 
+{
     vector<int> arr;
     int n, res;
     cout << "Enter the number of elements you want to enter: ";
@@ -20,7 +21,7 @@ int main() {
         cin >> res;
         arr[i] = res;
     }
-
+    // Quick Sort the array
     // Sort the vector in descending order using custom comparison function
     sort(arr.begin(), arr.end(), compare);
 
@@ -32,3 +33,18 @@ int main() {
 
     return 0;
 }
+
+/**
+ * 
+ * 
+ * sort: This is the standard library function std::sort from the <algorithm> header in C++. It is used to sort elements in a range defined by iterators.
+
+    arr.begin() and arr.end(): These are iterators that define the range of elements to be sorted. 
+    arr.begin() points to the beginning (first element) of the vector arr, and arr.end() points to one past the last element of the vector arr. 
+    This range [arr.begin(), arr.end()) includes all elements in the vector arr.
+
+    compare: This is a custom comparison function passed as the third argument to std::sort. 
+    The compare function is used to determine the order of elements during sorting. The function compare(i, j) should return false if i should go before j in the sorted sequence, and true otherwise.
+
+ * 
+ */
