9e53ee467d02f67489d1cd79645910224f5d163d
TheFatCow <50284597+TheFatCow@users.noreply.github.com>
2024-04-11 01:32:16 +0530

    done

---
+++ 12_Search_stdlib.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool compare(int i , int j)
+{
+   return (i < j);
+}
+
+int main()
+{
+    vector<int> arr;
+    int n,res;
+    cout<<"Enter the number of elements you want to input."<<endl;   
+    cin>>n;
+    cout<<"Enter "<<n<<" elements now"<<endl;
+    arr.resize(n);
+    for(int i = 0; i < n; i++)
+    {   
+        cin>>res;
+        arr[i] = res;
+    }
+
+    sort(arr.begin(), arr.end(), compare);
+
+    cout<<"Enter the number you ewant to find."<<endl;
+    cin>>res;
+
+    if (binary_search(arr.begin(),arr.end(),res)){
+        cout<<"Found "<<res<<endl;
+    }
+    else{
+        cout<<"Not found"<<endl;
+    }
+    return 0;
+}
