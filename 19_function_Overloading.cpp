da9db46730077a31ef793a53b46706a96b1a80e6
TheFatCow <50284597+TheFatCow@users.noreply.github.com>
2024-04-17 05:45:03 +0530

    done

--- 19_function_Overloading.cpp
+++ 19_function_Overloading.cpp
@@ -1,6 +1,9 @@
-/**Function overloading in C++ allows multiple functions within the same scope to have the same name but different parameter lists. 
+/*
+*Function overloading in C++ allows multiple functions within the same scope to have the same name but different parameter lists. 
  * This enables you to use the same function name to perform different tasks based on the type or number of arguments passed to the function. 
  * The compiler determines which overloaded function to call based on the arguments provided during the function call.
+ * This is also called as static polymorphism
+ * 
 
 Here's an example to demonstrate function overloading in C++: */
 
