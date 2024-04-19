a68b136bb3bcea02dcedbc11c9c017f4d5da42f9
TheFatCow <50284597+TheFatCow@users.noreply.github.com>
2024-04-19 15:24:22 +0530

    done

---
+++ 28_camel_casing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+# include<string>
+# include <algorithm>
+using namespace std;
+
+class camel{
+    private: string sentence;
+    public: void camel_casing(string mysentence)
+    {
+        sentence = mysentence;
+        sentence = sentence.
+    }
+};
